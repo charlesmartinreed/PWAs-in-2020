4ee77e3ba57c339f7bb6e166e54edaf6c90771ae
Charles <charlesmartinreed@icloud.com>
2020-06-02 18:26:29 -0500

    Add fake coffee data, render to DOM.

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector(".container");
+
+const coffeeOptions = [
+  { name: "Perspiciatis", image: "../images/coffee1.jpg" },
+  { name: "Voluptatem", image: "../images/coffee2.jpg" },
+  { name: "Explicabo", image: "../images/coffee3.jpg" },
+  { name: "Rchitecto", image: "../images/coffee4.jpg" },
+  { name: " Beatae", image: "../images/coffee5.jpg" },
+  { name: " Vitae", image: "../images/coffee6.jpg" },
+  { name: "Inventore", image: "../images/coffee7.jpg" },
+  { name: "Veritatis", image: "../images/coffee8.jpg" },
+  { name: "Accusantium", image: "../images/coffee9.jpg" },
+];
+
+const displayCoffeeOpts = () => {
+  let output = "";
+
+  coffeeOptions.forEach(
+    ({ name, image }) =>
+      (output += `
+        <div class="card">
+            <img class="card__avatar" src=${image} />
+            <h1 class="card__title">${name}</h1>
+            <a class="card__link" href="#">Order</a>
+        </div>
+        `)
+  );
+
+  // append the output to the container el on DOM
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", displayCoffeeOpts);
